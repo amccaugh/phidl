0671abc7e73266e1c264171b4e67a488d3203e38
Adam McCaughan <amccaugh@gmail.com>
2016-10-13 17:51:08 -0600

    Added distribute and movex/movey functions

--- phidl/device_layout.py
+++ phidl/device_layout.py
@@ -242,6 +242,7 @@ class Device(gdspy.Cell):
         l = self.add(gdspy.Label(text = text, position = position, anchor = 'o', layer=layer))
         return l
         
+
     def delete_port(self, name):
         self.ports.pop(name, None)
     
@@ -258,7 +259,26 @@ class Device(gdspy.Cell):
         sd = port.parent
         sd.connect(port, destination)
         return sd
-        
+
+
+    def distribute(self, elements, direction = 'x', spacing = 100, separation = True):
+        multiplier = 1
+        if   direction[0] == '+':
+            direction = direction[1:]
+        elif direction[0] == '-':
+            direction = direction[1:]
+            multiplier = -1
+
+        xy = np.array([0,0])
+        for e in elements:
+            e.center = xy
+            if direction == 'x':
+                xy = xy + (np.array([spacing, 0]) + np.array([e.xsize, 0])*(separation==True))*multiplier
+            elif direction == 'y':
+                xy = xy + (np.array([0, spacing]) + np.array([0, e.ysize])*(separation==True))*multiplier
+            else:
+                raise ValueError('[PHIDL] Distribute() needs a direction of "x", "+y", "-x", etc')
+
             
     def route(self, port1, port2, path_type = 'sine', width_type = 'straight', width1 = None, width2 = None, num_path_pts = 99, layer = 0, datatype = 0):
         # Assuming they're both Ports for now
@@ -530,6 +550,14 @@ class SubDevice(gdspy.CellReference):
 
         self.origin = np.array(self.origin) + np.array(d) - np.array(o)
         return self
+
+
+    def movex(self, origin = (0,0), destination = None):
+        self.move(origin = origin, destination = destination, axis = 'x')
+
+
+    def movey(self, origin = (0,0), destination = None):
+        self.move(origin = origin, destination = destination, axis = 'y')
         
         
     def rotate(self, angle = 45, center = (0,0)):
