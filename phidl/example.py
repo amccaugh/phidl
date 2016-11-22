4291187d57497eec627cc96f5fafe46f10e46534
Sonia Buckley <soniambuckley@gmail.com>
2016-11-21 20:27:52 -0700

    added adiabatic beamsplitter simpler version, and added LED

--- phidl/example.py
+++ phidl/example.py
@@ -13,6 +13,8 @@ import numpy as np
 import phidl.geometry as pg
 import basicPhotonics as bP
 
+#
+##
 interaction_length = 10
 gap1 = 1
 gap2 = 0.1
@@ -20,12 +22,10 @@ port1 = 0.4
 port2 = 0.5
 port3 = 0.4
 port4 = 0.4
-length_straight_biggapside = 5
-hsines = 3
-lsines = 6
-length_straight_aftersinebend = 2
-wWg = 0.7
-taper_length = 5
+hSines = 3
+rMin = 10
+lSines = np.sqrt((np.pi**2)*hSines*rMin/2)
+
 #
 D = Device("adiabatic beam splitter")
 #
@@ -39,50 +39,40 @@ xpts_lower = [0, 0, interaction_length, interaction_length]
 ypts_lower = [-gap1, -gap1-port2, -gap2-port4, -gap2]
 wg_lower = D.add_ref(bP.polygon(xcoords = xpts_lower, ycoords = ypts_lower, layer = 0))
 
-# add a straight section on the left
-wg_straight_biggapside1 = D.add_ref(pg.compass(size=[length_straight_biggapside, port1]))
-wg_straight_biggapside2 = D.add_ref(pg.compass(size=[length_straight_biggapside, port2]))
-
-# connect to the beamsplitter
-wg_straight_biggapside1.connect(port = 'E', destination = wg_upper.ports['0'])
-wg_straight_biggapside2.connect(port = 'E', destination = wg_lower.ports['0'])
-
-#draw straight sections that will come after the sinebends
-wg_straight_aftersinebend = D.add_ref(pg.compass(size=[length_straight_aftersinebend, port1]))
-wg_straight_aftersinebend2 = D.add_ref(pg.compass(size=[length_straight_aftersinebend, port2]))
-wg_straight_aftersinebend3 = D.add_ref(pg.compass(size=[length_straight_aftersinebend, port3]))
-wg_straight_aftersinebend4 = D.add_ref(pg.compass(size=[length_straight_aftersinebend, port4]))
-
 #locate the straight sections after the sine bends
-wg_straight_aftersinebend.center = [wg_straight_biggapside1.xmin-lsines, wg_straight_biggapside1.center[1]+hsines]
-wg_straight_aftersinebend2.center = [wg_straight_biggapside2.xmin-lsines, wg_straight_biggapside2.center[1]-hsines]
-wg_straight_aftersinebend3.center = [wg_upper.xmax+lsines, wg_upper.center[1]+hsines]
-wg_straight_aftersinebend4.center = [wg_lower.xmax+lsines, wg_lower.center[1]-hsines]
+P = Device('ports')
+P.add_port(name = 'port 1', midpoint = [wg_upper.xmin-lsines, wg_upper.center[1]+hsines], width = port1, orientation = 0)
+P.add_port(name = 'port 2', midpoint = [wg_lower.xmin-lsines, wg_lower.center[1]-hsines], width = port1, orientation = 0)
+P.add_port(name = 'port 3', midpoint = [wg_upper.xmax+lsines, wg_upper.center[1]+hsines], width = port1, orientation = 180)
+P.add_port(name = 'port 4', midpoint = [wg_lower.xmax+lsines, wg_lower.center[1]-hsines], width = port1, orientation = 180)
+route1 = D.add_ref(pg.route(port1 = P.ports['port 1'], port2 = wg_upper.ports['0'], path_type = 'sine'))
+route2 = D.add_ref(pg.route(port1 = P.ports['port 2'], port2 = wg_lower.ports['0'], path_type = 'sine'))
+route3 = D.add_ref(pg.route(port1 = P.ports['port 3'], port2 = wg_upper.ports['2'], path_type = 'sine'))
+route4 = D.add_ref(pg.route(port1 = P.ports['port 4'], port2 = wg_lower.ports['2'], path_type = 'sine'))
 
-#route with sine bends
-D.add_ref(pg.route(port1 = wg_straight_aftersinebend.ports['E'], port2 = wg_straight_biggapside1.ports['W'], path_type = 'sine'))
-D.add_ref(pg.route(port1 = wg_straight_aftersinebend2.ports['E'], port2 = wg_straight_biggapside2.ports['W'], path_type = 'sine'))
-D.add_ref(pg.route(port1 = wg_straight_aftersinebend3.ports['W'], port2 = wg_upper.ports['2'], path_type = 'sine'))
-D.add_ref(pg.route(port1 = wg_straight_aftersinebend4.ports['W'], port2 = wg_lower.ports['2'], path_type = 'sine'))
+D.add_port(port = route1.ports[1], name = 'port 1')
+D.add_port(port = route2.ports[1], name = 'port 2')
+D.add_port(port = route3.ports[1], name = 'port 3')
+D.add_port(port = route4.ports[1], name = 'port 4')
 
-#add tapers at either end
-T = pg.taper(length = taper_length, width1 = port1, width2 = wWg, layer = 0)
-t1 = D.add_ref(T)
-t2 = D.add_ref(T)
-t3 = D.add_ref(T)
-t4 = D.add_ref(T)
-
-t1.connect(port = 1, destination = wg_straight_aftersinebend.ports['W'])
-t2.connect(port = 1, destination = wg_straight_aftersinebend2.ports['W'])
-t3.connect(port = 1, destination = wg_straight_aftersinebend3.ports['E'])
-t4.connect(port = 1, destination = wg_straight_aftersinebend4.ports['E'])
-
-D.add_port(port = t1.ports[2], name = 'port 1')
-D.add_port(port = t2.ports[2], name = 'port 2')
-D.add_port(port = t3.ports[2], name = 'port 3')
-D.add_port(port = t4.ports[2], name = 'port 4')
 
 quickplot(D)
+
+#wg = D.add_ref(pg.compass(size=[lWg,wWg],layer=wgLayer))
+#wRegion = D.add_ref(pg.compass(size=[lWg,wE],layer=wLayer))
+#pRegion = D.add_ref(pg.compass(size=[lWg,wDope],layer=pLayer))
+#nRegion = D.add_ref(pg.compass(size=[lWg,wDope],layer=nLayer))
+#taper = D.add_ref(pg.taper(length = lTaper, width1 = wWg, width2 = wTaper))
+#
+#taper.xmin = wg.xmax
+#wg.connect(port = 'W', destination = taper.ports[1])
+#wg.center = wRegion.center
+#pRegion.ymin = wRegion.ymax + wDopeOffset
+#pRegion.center[0] = wRegion.center[0]
+#nRegion.ymax = wRegion.ymin - wDopeOffset
+#nRegion.center[0] = wRegion.center[0]
+#D.add_port(port = taper.ports[2], name = 'LED')
+#quickplot(D)
 
 # returns a polygon with ports on all edges
 #P = Device('polygon')
