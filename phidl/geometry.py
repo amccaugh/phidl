fc1e6a56f0d1e44be6ae521196a35b204944bde4
Adam McCaughan <amccaugh@gmail.com>
2017-09-12 14:24:56 -0600

    Minor edit

--- phidl/geometry.py
+++ phidl/geometry.py
@@ -1365,9 +1365,11 @@ def fill_rectangle(D, fill_size = (40,10), avoid_layers = 'all', include_layers 
     # Create the fill cell.  If fill_inverted is not specified, assume all False
     if fill_inverted is None: fill_inverted = [False]*len(fill_layers)
     if len(fill_layers) != len(fill_densities):
-        raise ValueError("[PHIDL] phidl.geometry.fill_rectangle() `fill_layers` and `fill_densities` parameters must be lists of the same length")
+        raise ValueError("[PHIDL] phidl.geometry.fill_rectangle() `fill_layers` and" +
+        " `fill_densities` parameters must be lists of the same length")
     if len(fill_layers) != len(fill_inverted):
-        raise ValueError("[PHIDL] phidl.geometry.fill_rectangle() `fill_layers` and `fill_inverted` parameters must be lists of the same length")
+        raise ValueError("[PHIDL] phidl.geometry.fill_rectangle() `fill_layers` and" +
+        " `fill_inverted` parameters must be lists of the same length")
 
     fill_cell = _fill_cell_rectangle(size = fill_size, layers = fill_layers,
                                      densities = fill_densities, inverted = fill_inverted)
