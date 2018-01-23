2845c2d53e8954162d3b3e05cf7a3baf27c21428
amccaugh <amccaugh@gmail.com>
2018-01-22 18:25:15 -0700

    Updated local testing script

--- local_testing_script_reload.py
+++ local_testing_script_reload.py
@@ -12,7 +12,7 @@ sys.path.insert(0, local_phidl_path)
 
 #%% Import as usual
 
-from phidl import Device, Layer, quickplot2 as qp, make_device as md
+from phidl import Device, Layer, make_device as md, quickplot2 as qp
 import phidl.geometry as pg, phidl.routing as pr
 import numpy as np
 from phidl.device_layout import DeviceReference
@@ -26,16 +26,14 @@ importlib.reload(phidl)
 importlib.reload(phidl.device_layout)
 importlib.reload(phidl.geometry)
 importlib.reload(phidl.routing)
+Device = phidl.device_layout.Device
+DeviceReference = phidl.device_layout.DeviceReference
 
 print(phidl.__version__)
 
 #%%
-
+D = Device()
 D = pg.import_gds(r'C:\Users\amcc\Documents\GitHub\die-designs\gds-files\SE005 SNSPD Integrator.gds', flatten = False)
-
-
-#%%
-
-# pg.copy(D, flatten = true, single_layer = None)
-
+p = D.add_polygon([[100,200,300,400],[500,600,900,1000]], layer = 5)
+label = D.label('hello kitty2')
 
