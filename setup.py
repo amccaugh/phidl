2b664f2ba73f3a98a5a44d020b34b225f525e5fd
Adam McCaughan <amccaugh@gmail.com>
2018-06-20 18:03:20 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires=[
 ]
 
 setup(name='phidl',
-      version='0.8.5',
+      version='0.8.6',
       description='PHIDL',
       install_requires=install_requires,
       author='Adam McCaughan',
