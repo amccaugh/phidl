cec1bc3d054f2d1f86d8b139ccd503b39ecb7740
Alex Tait <alexander.tait@nist.gov>
2018-07-12 18:16:11 -0600

    notebook is cleaning up files

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -6,14 +6,14 @@ import os
 def test_tutorial():
     matplotlib.rcParams['figure.max_open_warning'] = 0  # prevents a memory warning
 
-    # Dirty clean up 1: store the initial file snapshot
+    # Artifact clean up 1: store the initial file snapshot
     initialFiles = []
     for fname in os.listdir('.'):
         initialFiles.append(fname)
 
     import phidl.phidl_tutorial_example  # importing runs the module code
 
-    # Dirty clean up 2: delete any new files
+    # Artifact clean up 2: delete any new files
     for fname in os.listdir('.'):
         if fname not in initialFiles:
             os.remove(fname)
