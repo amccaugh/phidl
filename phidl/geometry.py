a18d9411c3c4291e0a96ffbae20eb215e859f725
Adam McCaughan <amccaugh@gmail.com>
2019-10-27 19:01:49 -0600

    Working offset with num_divisions parameter

--- phidl/geometry.py
+++ phidl/geometry.py
@@ -6,6 +6,7 @@ from numpy import sqrt, pi, cos, sin, log, exp, sinh
 # from scipy.interpolate import interp1d
 
 import gdspy
+from gdspy import clipper
 from phidl.device_layout import Device, Port, Polygon, CellArray
 from phidl.device_layout import _parse_layer, DeviceReference
 import copy as python_copy
@@ -359,7 +360,8 @@ def C(width = 1, size = (10,20) , layer = 0):
 #
 #==============================================================================
 
-def offset(elements, distance = 0.1, join_first = True, precision = 1e-6, layer = 0):
+def offset(elements, distance = 0.1, join_first = True, precision = 1e-6, 
+        num_divisions = [1,1], layer = 0):
     if type(elements) is not list: elements = [elements]
     polygons_to_offset = []
     for e in elements:
@@ -367,9 +369,20 @@ def offset(elements, distance = 0.1, join_first = True, precision = 1e-6, layer 
         elif isinstance(e, (Polygon, gdspy.Polygon)): polygons_to_offset.append(e)
     polygons_to_offset = _merge_floating_point_errors(polygons_to_offset, tol = 1e-10)
     gds_layer, gds_datatype = _parse_layer(layer)
-    p = gdspy.offset(polygons_to_offset, distance = distance, join='miter', tolerance=2,
-                     precision=precision, join_first=join_first,
-                     max_points=4000, layer=gds_layer, datatype = gds_datatype)
+    if all(np.array(num_divisions) == np.array([1,1])):
+        p = gdspy.offset(polygons_to_offset, distance = distance, join='miter', tolerance=2,
+                         precision=precision, join_first=join_first,
+                         max_points=4000, layer=gds_layer, datatype = gds_datatype)
+    else:
+        p = _offset_polygons_parallel(
+            polygons_to_offset,
+            distance = distance,
+            num_divisions = num_divisions,
+            join_first = join_first,
+        #    max_points = 4000,
+            precision = precision,
+            )
+
     D = Device('offset')
     D.add_polygon(p, layer=layer)
     return D
@@ -520,7 +533,7 @@ def _merge_floating_point_errors(polygons, tol = 1e-10):
     xfixed = _merge_nearby_floating_points(x, tol = tol)
     yfixed = _merge_nearby_floating_points(y, tol = tol)
     stacked_polygons_fixed = np.vstack([xfixed, yfixed]).T
-    polygons_fixed = np.vsplit(stacked_polygons_fixed, polygon_indices)
+    polygons_fixed = np.vsplit(stacked_polygons_fixed, polygon_indices[:-1])
     return polygons_fixed
 
 
@@ -541,6 +554,130 @@ def _merge_nearby_floating_points(x, tol = 1e-10):
     for xi in xsortthresholdind:
          xsort[xi+1] = xsort[xi]
     return xsort[xargunsort]
+
+
+def _crop_region(polygons, left, bottom, right, top, precision):
+    """ Given a rectangular boundary defined by left/bottom/right/top, this
+    takes a list of polygons and cuts them at the boundary, discarding parts
+    of the polygons outside the rectangle. """
+    cropped_polygons = []
+    for p in polygons:
+        clipped_polys = clipper._chop(p, [top, bottom], 1, 1 / precision) # polygon, [cuts], axis, scale
+        for cp in clipped_polys[1]:
+            result = clipper._chop(cp, [left, right], 0, 1 / precision)
+            cropped_polygons += list(result[1])
+    return cropped_polygons
+
+
+
+def _find_bboxes_in_rect(bboxes, left, bottom, right, top):
+    """ Given a list of polygon bounding boxes and a rectangle defined by
+    left/bottom/right/top, this function returns those polygons which overlap
+    the rectangle. """
+    result = (bboxes[:,0] <= right) & (bboxes[:,2] >= left) & \
+             (bboxes[:,1] <= top)   & (bboxes[:,3] >= bottom)
+    return result
+
+# _find_bboxes_on_rect_edge
+def _find_bboxes_on_rect_edge(bboxes, left, bottom, right, top):
+    """ Given a list of polygon bounding boxes and a rectangular boundary defined by
+    left/bottom/right/top, this function returns those polygons which intersect
+    the rectangular boundary. """
+    bboxes_left   = _find_bboxes_in_rect(bboxes, left, bottom, left, top)
+    bboxes_right  = _find_bboxes_in_rect(bboxes, right, bottom, right, top)
+    bboxes_top    = _find_bboxes_in_rect(bboxes, left, top, right, top)
+    bboxes_bottom = _find_bboxes_in_rect(bboxes, left, bottom, right, bottom)
+    result = bboxes_left | bboxes_right | bboxes_top | bboxes_bottom
+    return result
+
+
+def _offset_region(all_polygons, bboxes, left, bottom, right, top,
+                distance = 5,
+                join_first = True,
+#                max_points = 4000,
+                precision = 1e-6,
+                ):
+    """ Taking a region of e.g. size (x,y) which needs to be offset by distance d,
+    this function crops out a region (x+2*d, y+2*d) large, offsets that region,
+    then crops it back to size (x,y) to create a valid result """
+        
+    # Mark out a region slightly larger than the final desired region
+    # FIXME: Necessary?
+    d = distance*1.01
+    
+    # Parse out which polygons are along the edge of the rectangle and need to be 
+    # cropped and which are deep inside the rectangle region and can be left alone
+    polygons_in_rect_i = _find_bboxes_in_rect(bboxes, left-d, bottom-d, right+d, top+d)
+    polygons_edge_i = _find_bboxes_on_rect_edge(bboxes, left-d, bottom-d, right+d, top+d)
+    polygons_in_rect_no_edge_i = polygons_in_rect_i & (~polygons_edge_i)
+    
+    # Crop polygons along the edge and recombine them with polygons inside the rectangle
+    polygons_edge = all_polygons[polygons_edge_i]
+    polygons_in_rect_no_edge = all_polygons[polygons_in_rect_no_edge_i].tolist()
+    polygons_edge_cropped = _crop_region(polygons_edge, left-d, bottom-d, right+d, top+d, precision = precision)
+    polygons_to_offset = polygons_in_rect_no_edge + polygons_edge_cropped
+    
+    # Offset the resulting cropped polygons and recrop to final desired size
+    polygons_offset = clipper.offset(polygons_to_offset, distance, 'miter', 2, 1/precision, int(join_first))
+    polygons_offset_cropped = _crop_region(polygons_offset, left, bottom, right, top, precision = precision)
+    
+    return polygons_offset_cropped
+
+
+def _polygons_to_bboxes(polygons):
+#    Build bounding boxes
+    bboxes = np.empty([len(polygons),4])
+    for n,p in enumerate(polygons):
+        try:
+            left,bottom = np.min(p,axis = 0)
+        except:
+            import pdb
+            pdb.set_trace()
+        right,top = np.max(p,axis = 0)
+        bboxes[n] = [left, bottom, right, top]
+    return bboxes
+
+def _offset_polygons_parallel(
+    polygons,
+    distance = 5,
+    num_divisions = [10,10],
+    join_first = True,
+#    max_points = 4000,
+    precision = 1e-6,
+    ):
+    
+#    Build bounding boxes
+    polygons = np.asarray(polygons)
+    bboxes = _polygons_to_bboxes(polygons)
+    
+    xmin,ymin = np.min(bboxes[:,0:2], axis = 0)
+    xmax,ymax = np.max(bboxes[:,2:4], axis = 0)
+    
+    xsize = xmax - xmin
+    ysize = ymax - ymin
+    xdelta = xsize/num_divisions[0]
+    ydelta = ysize/num_divisions[1]
+    xcorners = xmin + np.arange(num_divisions[0])*xdelta
+    ycorners = ymin + np.arange(num_divisions[1])*ydelta
+
+    offset_polygons = []
+    for n,xc in enumerate(xcorners):
+        for m,yc in enumerate(ycorners):
+            left = xc
+            right = xc+xdelta
+            bottom = yc
+            top = yc+ydelta
+            _offset_region_polygons = _offset_region(polygons, bboxes,
+                                            left, bottom, right, top,
+                                            distance = distance,
+                                            join_first = join_first,
+#                                            max_points = max_points,
+                                            precision = precision,
+                                            )
+            offset_polygons += _offset_region_polygons
+            
+    return offset_polygons
+
 
 
 #==============================================================================
