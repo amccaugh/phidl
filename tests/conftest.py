ce1534ed8f21b7ea9ca86b5a82cb78df8ca09fb5
Alex <alexander.tait@nist.gov>
2020-01-22 20:50:27 -0700

    Comment in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+''' This runs before every test, even if they happen in parallel.
+    Use it for setting up a consistent state of global variables or setting up directories
+'''
 # make sure phidl is on path
 import os, sys
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
