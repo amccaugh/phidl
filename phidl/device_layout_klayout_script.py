e395ece9280cc0959ed8473d3062b6e1fb467ce7
amccaugh <amccaugh@gmail.com>
2020-03-21 19:46:14 -0600

    Update device_layout_klayout_script.py

--- phidl/device_layout_klayout_script.py
+++ phidl/device_layout_klayout_script.py
@@ -29,6 +29,7 @@ transformation = kdb.CplxTrans(
 new_poly.transform(transformation)
 
 
+
 # Create layer https://www.klayout.de/doc-qt5/code/class_Layout.html#m_layer
 new_layer = layout.layer(1,6) # new_layer represents the *index* of the relevant layer in the layout's layer list
 new_layer2 = layout.layer(3,7) 
@@ -39,7 +40,7 @@ grab_layer = layout.layer(3,7)
 
 
 
-# Add polygon
+# Add polygon to the cell
 p1 = new_cell.shapes(new_layer).insert(new_poly)
 p2 = new_cell.shapes(new_layer).insert(new_poly.transform(transformation))
 p3 = new_cell.shapes(new_layer2).insert(new_poly.transform(transformation))
@@ -48,7 +49,7 @@ p3 = new_cell.shapes(new_layer2).insert(new_poly.transform(transformation))
 # Iterate through polygons in one layer of a cell
 list(new_cell.each_shape(new_layer))
 
-# Iterate through all polygons in a cell (get_polygons())
+# Iterate through all polygons in a cell (get_polygons()) https://www.klayout.de/doc/code/class_Cell.html#method9
 all_polygons_iterator = new_cell.begin_shapes_rec(new_layer)
 while not all_polygons_iterator.at_end():
     polygon = all_polygons_iterator.shape()
@@ -56,6 +57,22 @@ while not all_polygons_iterator.at_end():
     all_polygons_iterator.next()
 
 # List all layers in use
+
+#%% Functions
+
+
+
+def rotate(polygon, angle):
+    # Transform polygon
+    transformation = kdb.CplxTrans(
+            1,  # Magnification
+            angle,  # Rotation
+            False,# X-axis mirroring
+            0, # X-displacement
+            0,  # Y-displacement
+            )
+    polygon.transform(transformation)
+    return polygon
 
 
 #%% Notes
