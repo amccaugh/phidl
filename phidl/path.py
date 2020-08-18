99afce1d09d48b8040461a6b7a67e8ad199424e0
amccaugh <amccaugh@gmail.com>
2020-08-18 11:50:25 -0600

    Update path.py

--- phidl/path.py
+++ phidl/path.py
@@ -2,27 +2,34 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
 import warnings
+from phidl.device_layout import Path
 
 
 def arc(radius = 10, angle = 90, num_pts = 720):
-    """ Produces an arc of points with `num_pts` per 360 degrees.  An extra point is
-    tacked on each end to ensure that the numerical gradient is accurate """
-    t = np.linspace(0, angle*np.pi/180, abs(int(num_pts*angle/360))-2)
+    """ Produces an arc of points with `num_pts` per 360 degrees """
+    num_pts = abs(int(num_pts*angle/360))
+    t = np.linspace(-90*np.pi/180, (angle-90)*np.pi/180, num_pts)
     x = radius*np.cos(t)
-    y = radius*np.sin(t)
+    y = radius*(np.sin(t)+1)
     points = np.array((x,y)).T
 
-    start_angle = 90*np.sign(angle)
-    end_angle = start_angle + angle
-    
-    return points, start_angle, end_angle
+    P = Path()
+    # Manually add points & adjust start and end angles
+    P.points = points
+    P.start_angle = 180*(angle<0)
+    P.end_angle = P.start_angle + angle
+    return P
 
 
 def straight(length = 5, num_pts = 100):
     """ Produces an straight section """
     x = np.linspace(0, length, num_pts)
     y = x*0
-    return np.array((x,y)).T
+    points = np.array((x,y)).T
+    
+    P = Path()
+    P.append(points)
+    return P
 
 
 #### Euler bends
@@ -32,11 +39,12 @@ def _cumtrapz(x):
     function usually found in scipy (scipy.integrate.cumtrapz) """
     return np.cumsum((x[1:] + x[:-1])/2)
 
-def partial_euler(Rmin = 3, a = 90, p = 0.2, num_pts = 4000):
+def partial_euler(angle = 90, Rmin = 3, p = 0.2, num_pts = 720):
+    num_pts = abs(int(num_pts*angle/360))
     # Overhead calculations
-    a = np.radians(a)
-    sp = np.sqrt(p*a)      # Clothoid-to-normal transition point s value
-    s0 = a*Rmin + sp    # Total path length derived from curvature integral = a
+    angle = np.radians(angle)
+    sp = np.sqrt(p*angle)      # Clothoid-to-normal transition point s value
+    s0 = angle*Rmin + sp    # Total path length derived from curvature integral = angle
     eta = 1 / (2*sp*Rmin) # Scaling factor to enforce Rmin
     # Constructing s and K arrays
     s = np.linspace(0, s0, num_pts)
@@ -51,9 +59,14 @@ def partial_euler(Rmin = 3, a = 90, p = 0.2, num_pts = 4000):
                                             2*(s0 - s[i2:num_pts]))])
     # Integrating to find x and y
     ds = s[1] - s[0]
-    phi = cumtrapz(K*ds)
+    phi = _cumtrapz(K*ds)
     x, y = np.concatenate([np.array([[0],[0]]), 
-                           np.cumsum([ds*cos(phi), ds*sin(phi)], axis = 1)],
-                          axis = 1)
-    return x, y
+                           np.cumsum([ds*np.cos(phi),
+                           ds*np.sin(phi)], axis = 1)],
+                           axis = 1)
+    points = np.array((x,y)).T
+
+    P = Path()
+    P.append(points)
+    return P
 
