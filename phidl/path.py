b7f887d9cf6733d42ea204ef4a4b6c4f62aefe84
Adam McCaughan <amccaugh@gmail.com>
2020-09-13 19:33:49 -0600

    Final of euler() with scaling

--- phidl/path.py
+++ phidl/path.py
@@ -8,7 +8,7 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
 import warnings
-from phidl.device_layout import Path
+from phidl.device_layout import Path, _rotate_points
 
 
 def arc(radius = 10, angle = 90, num_pts = 720):
@@ -74,20 +74,42 @@ def _cumtrapz(x):
     return np.cumsum((x[1:] + x[:-1])/2)
 
 
-def euler(Rmin = 3, angle = 90, p = 1.0, num_pts = 720):
-    """ Create an Euler curve (also known as "racetrack" or "clothoid" curve)
-    with smoothly varying curvature and with a miminum radius of curvature equal
-    to Rmin.  If p < 1.0, will create a "partial euler" curve as described in
-    Vogelbacher et. al. https://dx.doi.org/10.1364/oe.27.031394
+def _fresnel(R0, s, num_pts, n_iter=8):
+    """ Fresnel integral using a series expansion """
+    t = np.linspace(0,s/(np.sqrt(2)*R0), num_pts)
+    x = np.zeros(num_pts)
+    y = np.zeros(num_pts)
+    
+    for n in range(0,n_iter):
+      x += (-1)**n * t**(4*n+1)/(np.math.factorial(2*n) * (4*n+1))
+      y += (-1)**n * t**(4*n+3)/(np.math.factorial(2*n+1) * (4*n+3))
+    
+    x = np.concatenate([[0], x])
+    y = np.concatenate([[0], y])
+    return np.array([np.sqrt(2)*R0*x, np.sqrt(2)*R0*y])
+
+
+def euler(radius = 3, angle = 90, p = 1.0, use_eff = False, num_pts = 720):
+    """ Create an Euler bend (also known as "racetrack" or "clothoid" curve)
+    with smoothly varying curvature.  By default, `radius` corresponds to the
+    minimum radius of curvature of the bend. However, if `use_eff` is set to
+    True, `radius` corresponds to the effective radius of curvature (making the
+    curve a drop-in replacement for an arc). If p < 1.0, will create a "partial
+    euler" curve as described in Vogelbacher et. al.
+    https://dx.doi.org/10.1364/oe.27.031394
 
     Parameters
     ----------
     angle : int or float
         Total angle of curve
-    Rmin : int or float
+    radius : int or float
         Minimum radius of curvature
     p : float
         Proportion of curve that is an Euler curve
+    use_eff : bool
+        If False: `radius` corresponds to minimum radius of curvature of the bend
+        If True:  `radius` corresponds to the end-to-end radius of curvature that
+        an equivalent circular arc would have
     num_pts : int
         Number of points used per 360 degrees
 
@@ -99,32 +121,59 @@ def euler(Rmin = 3, angle = 90, p = 1.0, num_pts = 720):
     if (p < 0) or (p > 1):
         raise ValueError('[PHIDL] euler() requires argument `p` be between 0 and 1')
 
-    num_pts = abs(int(num_pts*angle/360))
-    num_pts_euler = int(np.round(num_pts/2*p))
+    R0 = 1
+    alpha = np.radians(angle)
+    Rp = R0 / (np.sqrt(p*alpha))
+    sp = R0 * np.sqrt(p*alpha)
+    s0 = 2*sp + Rp*alpha*(1-p)
+    num_pts_euler = int(np.round(sp/(s0/2)*num_pts))
+    num_pts_arc = num_pts - num_pts_euler
 
-    K1 = np.linspace(0, 1/Rmin, num_pts_euler)
-    K2 = np.ones(num_pts - 2*num_pts_euler)/Rmin
-    K3 = np.linspace(1/Rmin, 0, num_pts_euler)
-    K = np.concatenate([K1,K2,K3])
-    # Integrating to find x and y
-    ds = np.radians(angle)/np.trapz(K)
-    phi = _cumtrapz(K*ds)
-    x, y = np.concatenate([np.array([[0],[0]]), 
-                        np.cumsum([ds*np.cos(phi),
-                        ds*np.sin(phi)], axis = 1)],
-                        axis = 1)
-    points = np.array((x,y)).T
+    xbend1, ybend1 = _fresnel(R0, sp, num_pts_euler)
+    xp, yp = xbend1[-1], ybend1[-1]
+
+    dx = xp - Rp*np.sin(p*alpha/2)
+    dy = yp - Rp*(1-np.cos(p*alpha/2))
+
+    s = np.linspace(sp, s0/2, num_pts_arc)
+    xbend2 = Rp*np.sin((s-sp)/Rp + p*alpha/2) + dx
+    ybend2 = Rp*(1 - np.cos((s-sp)/Rp + p*alpha/2)) + dy
+
+    x = np.concatenate([xbend1, xbend2[1:]])
+    y = np.concatenate([ybend1, ybend2[1:]])
+    points1 = np.array([x,y]).T
+    points2 = np.flipud(np.array([x,-y]).T)
+
+    points2 = _rotate_points(points2, angle-180)
+    points2 += -points2[0,:] + points1[-1,:]
+
+    points = np.concatenate([points1[:-1],points2])
+
+    # Find y-axis intersection point to compute Reff
+    start_angle = 180*(angle<0)
+    end_angle = start_angle + angle
+    dy = np.tan(np.radians(end_angle-90)) * points[-1][0]
+    Reff = points[-1][1] - dy
+    Rmin = Rp
+    
+    # Fix degenerate condition at angle == 180
+    if np.abs(180-angle) < 1e-3:
+        Reff = points[-1][1]/2
+
+    # Scale curve to either match Reff or Rmin
+    if use_eff == True:
+        scale = radius/Reff
+    else:
+        scale = radius/Rmin
+    points *= scale
 
     P = Path()
     # Manually add points & adjust start and end angles
     P.points = points
-    P.start_angle = 180*(angle<0)
-    P.end_angle = P.start_angle + angle
-
-    # Compute Reff using the fact that it starts at (0,0) with an angle 0
-    dy = np.tan(np.radians(P.end_angle-90)) * points[-1][0]
-    P.info['Reff'] = points[-1][1] - dy
-
+    P.start_angle = start_angle
+    P.end_angle = end_angle
+    P.info['Reff'] = Reff*scale
+    P.info['Rmin'] = Rmin*scale
     return P
 
 
