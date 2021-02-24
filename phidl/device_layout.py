d7ca460c01650772cbb1f6401b5dea62f4a88ff1
amccaugh <amccaugh@gmail.com>
2021-02-23 21:03:06 -0700

    Version bump

--- phidl/device_layout.py
+++ phidl/device_layout.py
@@ -59,7 +59,7 @@ from phidl.constants import _CSS3_NAMES_TO_HEX
 import gdspy.library
 gdspy.library.use_current_library = False
 
-__version__ = '1.4.3'
+__version__ = '1.4.4'
 
 
 #==============================================================================
