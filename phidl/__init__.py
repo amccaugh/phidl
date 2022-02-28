5e98e5b282dbd8daf242aa530dcce7ca7f9a11b6
Bas Nijholt <bas@nijho.lt>
2022-02-28 14:50:46 -0800

    add __all__

--- phidl/__init__.py
+++ phidl/__init__.py
@@ -12,3 +12,20 @@ from phidl.device_layout import (
 )
 from phidl.geometry import device_lru_cache
 from phidl.quickplotter import quickplot, quickplot2, set_quickplot_options
+
+__all__ = [
+    "CrossSection",
+    "Device",
+    "Group",
+    "Layer",
+    "LayerSet",
+    "Path",
+    "Port",
+    "__version__",
+    "make_device",
+    "reset",
+    "device_lru_cache",
+    "quickplot",
+    "quickplot2",
+    "set_quickplot_options",
+]
