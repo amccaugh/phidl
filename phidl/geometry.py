fix edge case for numsquares calculation when start_width==end_width for optimal_step

phidl/geometry.py
@@ -4636,8 +4636,6 @@ def optimal_step(start_width = 10, end_width = 22, num_pts = 50,
 
         ypts[-1] = end_width
         ypts[0] = start_width
-        y_num_sq = np.array(ypts)
-        x_num_sq = np.array(xpts)
 
         if symmetric == False:
             xpts.append(xpts[-1])
@@ -4677,6 +4675,8 @@ def optimal_step(start_width = 10, end_width = 22, num_pts = 50,
         D.add_port(name = 2, midpoint = [max(xpts), 0], width = end_width,
                    orientation = 0)
 
+    y_num_sq = np.array(ypts)
+    x_num_sq = np.array(xpts)
     D.info['num_squares'] = np.sum(np.diff(x_num_sq) /((y_num_sq[:-1] + y_num_sq[1:])/2) )
     return D
 