1f54e01dbd1eda77d6ef7a613dc09c46d97f8430
Adam McCaughan <amccaugh@users.noreply.github.com>
2022-04-07 14:56:46 -0600

    Update version

--- phidl/device_layout.py
+++ phidl/device_layout.py
@@ -51,7 +51,7 @@ from phidl.constants import _CSS3_NAMES_TO_HEX
 
 gdspy.library.use_current_library = False
 
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 
 
 # ==============================================================================
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="phidl",
-    version="1.6.0",
+    version="1.6.1",
     description="PHIDL",
     long_description=long_description,
     long_description_content_type="text/markdown",
