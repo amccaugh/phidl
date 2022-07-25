a3900bad09965fccf4783568bd8fab827039fa29
Adam McCaughan <amccaugh@users.noreply.github.com>
2022-07-24 20:57:47 -0600

    Update version

--- phidl/device_layout.py
+++ phidl/device_layout.py
@@ -52,7 +52,7 @@ from phidl.constants import _CSS3_NAMES_TO_HEX
 
 gdspy.library.use_current_library = False
 
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 
 
 # ==============================================================================
