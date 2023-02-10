9d64f1cb8cefc8589729d495b36f3eb5abd93c51
Adam McCaughan <amccaugh@users.noreply.github.com>
2023-02-09 20:34:48 -0700

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="phidl",
-    version="1.6.2",
+    version="1.6.3",
     description="PHIDL",
     long_description=long_description,
     long_description_content_type="text/markdown",
